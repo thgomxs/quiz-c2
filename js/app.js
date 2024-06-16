3a551e667795b8fb21e8af88e5116f43503da2f6
thgomxs <thiagogomespereiradeabreu@gmail.com>
2024-06-16 13:33:35 -0300

    Questões adicionadas e atualizadas

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
-
 let perguntaIndex = 0
+
 let respostaDetalhes = {
     ordem: null,
     linear: null,
-    homogenea: null
+    homogenea: null,
+    coeficientes: null,
+    tipo: null,
+    raizes: null,
 }
 let respostaAtual = null
 let perguntaAtual = null
 
+const quizResultadoSolucoes = document.querySelectorAll(".quiz-resultado-solucao")
 const quizNome = document.querySelector("#quiz-nome")
-const quizResultado = document.querySelector("#quiz-resultado")
 const quizReplayButton = document.querySelector("#quiz-replay-btn")
 
 const inicarButton = document.querySelector('#iniciar-btn');
@@ -21,6 +24,8 @@ const perguntaTitulo = document.querySelector('#pergunta-titulo');
 const escolhas = document.querySelectorAll('.escolha');
 const escolhaA = document.querySelector('#escolha-a');
 const escolhaB = document.querySelector('#escolha-b');
+const escolhaC = document.querySelector('#escolha-c');
+const escolhaD = document.querySelector('#escolha-d');
 
 
 
@@ -39,58 +44,108 @@ const perguntas = [
     titulo: 'Quando te convidam para sair, você sempre diz \'sim\' de maneira uniforme ou sua resposta varia dependendo de outros fatores como o clima e seu humor?',
     tipo: 'homogenea',
     opcoes: {a: 'Sempre "sim"', b: 'Depende de fatores'}
+    },
+    {
+    titulo: 'Você costuma se manter firme e constante nas suas opiniões ou você deixa elas serem influenciadas e alteradas pelo meio e por circunstâncias externas?',
+    tipo: 'coeficientes',
+    opcoes: {a: 'Me mantenho constante', b: 'Mudo conforme as circunstâncias'}
+    },
+    {
+    titulo: 'Qual atitude você tomaria caso visse dois de seus amigos brigando?',
+    tipo: 'tipo',
+    opcoes: {a: 'Tentaria separar a briga', b: 'Correria da briga ', c: 'Tentaria ser imparcial com ambos os lados da discussão', d: 'Seria parcial para um lado'}
+    },
+    {
+    titulo:'Você costuma ser uma pessoa otimista, neutra ou pessimista?',
+    tipo: 'raizes',
+    opcoes: {a: 'Otimista', b: 'Neutra', c: 'Pessimista'}
     }
 ]
 
 let respostas = [
     {
         nome: 'EDO LINEAR NÃO HOMOGENEA DE PRIMEIRA ORDEM',
+        contador: 0,
         linear: true,
         ordem: 1,
-        homogenea: false
+        homogenea: false,
     },
     {
         nome: 'EDO LINEAR HOMOGENEA DE PRIMEIRA ORDEM',
+        contador: 0,
         linear: true,
         ordem: 1,
-        homogenea: true
+        homogenea: true,
+        coeficientes: null,
     },
     {
-        nome: 'EDO NÃO LINEAR HOMOGENEA DE PRIMEIRA ORDEM',
+        nome: 'EDO NÃO LINEAR DE PRIMEIRA ORDEM',
+        contador: 0,
         linear: false,
         ordem: 1,
-        homogenea: true
-    },
-    {
-        nome: 'EDO NÃO LINEAR NÃO HOMOGENEA DE PRIMEIRA ORDEM',
-        linear: false,
-        ordem: 1,
-        homogenea: false
+        homogenea: false,
+        coeficientes: null,
     },
     {
         nome: 'EDO LINEAR HOMOGENEA DE SEGUNDA ORDEM',
+        contador: 0,
         linear: true,
         ordem: 2,
-        homogenea: true
+        homogenea: true,
+        coeficientes: null,
     },
     {
         nome: 'EDO LINEAR NÃO HOMOGENEA DE SEGUNDA ORDEM',
+        contador: 0,
         linear: true,
         ordem: 2,
-        homogenea: false
+        homogenea: false,
+        coeficientes: null,
     }
 ]
 
 
 
-
 function mostrarResultado(){
-    if (respostas.length === 1){
-        quizResultado.innerHTML = `VOCÊ É UMA <span class="text-primary"> ${respostas[0].nome}</span>`
-        quizResultado.style.display = 'block'
-        perguntaContainer.style.display = 'none'
-        quizReplayButton.style.display = 'block'
+    let resultado = null
+    let maiorContador = 0
+    respostas.forEach((resposta)=>{
+        if (resposta.contador > maiorContador){
+            maiorContador = resposta.contador
+            resultado = resposta
+        }
+    })
+
+    if (respostaDetalhes.ordem === 1 && respostaDetalhes.homogenea === false){
+        if (respostaDetalhes.coeficientes === true){
+            document.querySelector("#edo-nao-homogenea").innerHTML += " COM COEFICIENTES CONSTANTES"
+        }
+        if (respostaDetalhes.coeficientes === false){
+            document.querySelector("#edo-nao-homogenea").innerHTML += " COM COEFICIENTES VARIÁVEIS"
+        }
     }
+
+    if (respostaDetalhes.ordem === 1 && respostaDetalhes.linear === false){
+        document.querySelector("#edo-nao-linear").innerHTML += ` ${respostaDetalhes.tipo}`
+    }
+
+    if (respostaDetalhes.ordem === 2 && respostaDetalhes.homogenea === true){
+        document.querySelector("#edo-raizes").innerHTML += ` COM COEFICIENTES CONSTANTES E ${respostaDetalhes.raizes}`
+    }
+
+
+    quizResultadoSolucoes.forEach((solucao)=>{
+        solucao.querySelector('h1').innerHTML = `Parabéns! Você recebeu: <span class="text-primary">${resultado.nome}</span>`
+
+        let nomeEdo = solucao.getAttribute("data-edo")
+        console.log(nomeEdo, resultado.nome, nomeEdo === resultado.nome)
+        if (nomeEdo === resultado.nome){
+            solucao.style.display = "flex"
+        }
+    })
+    perguntaContainer.style.display = 'none'
+    quizReplayButton.style.display = 'block'
+
 }
 
 function limparRespostas(){
@@ -105,34 +160,49 @@ function atualizarPergunta() {
     perguntaAtual = perguntas[perguntaIndex]
 
     perguntaTitulo.innerHTML = `${perguntaIndex + 1}. ${perguntaAtual.titulo}`
+
     escolhaA.innerHTML = perguntaAtual.opcoes.a
     escolhaB.innerHTML = perguntaAtual.opcoes.b
+
+    if (perguntaIndex === 4){
+        escolhaC.style.display = 'block'
+        escolhaD.style.display = 'block'
+        escolhaC.innerHTML = perguntaAtual.opcoes.c
+        escolhaD.innerHTML = perguntaAtual.opcoes.d
+    } else if (perguntaIndex === 5){
+        escolhaC.style.display = 'block'
+        escolhaD.style.display = 'none'
+        escolhaC.innerHTML = perguntaAtual.opcoes.c
+    } else{
+        escolhaC.style.display = 'none'
+        escolhaD.style.display = 'none'
+    }
+
 }
 atualizarPergunta()
 
 
 function proximaPergunta(){
     perguntaIndex++
-    console.log(perguntaIndex, (perguntas.length - 1))
 
-    let apagar = []
-    console.log(respostaAtual, perguntaAtual)
     if (perguntaAtual.tipo === 'ordem'){
         if (respostaAtual === 'A'){
             respostas.forEach((resposta)=>{
-                if (resposta.ordem === 2){
-                    apagar.push(resposta.nome)
+                if (resposta.ordem === 1){
+                    resposta.contador += 1
                 }
             })
             respostaDetalhes.ordem = 1
         }
         if (respostaAtual === 'B'){
             respostas.forEach((resposta)=>{
-                if (resposta.ordem === 1){
-                    apagar.push(resposta.nome)
+                if (resposta.ordem === 2){
+                    resposta.contador += 1
                 }
             })
             respostaDetalhes.ordem = 2
+            respostaDetalhes.linear = true
+            respostaDetalhes.coeficientes = true
         }
     }
 
@@ -140,17 +210,16 @@ function proximaPergunta(){
         if (respostaDetalhes.ordem !== 2){
             if (respostaAtual === 'A'){
                 respostas.forEach((resposta)=>{
-                    if (resposta.linear === false){
-                        apagar.push(resposta.nome)
-
+                    if (resposta.linear === true){
+                        resposta.contador += 1
                     }
                 })
                 respostaDetalhes.linear = true
             }
             if (respostaAtual === 'B'){
                 respostas.forEach((resposta)=>{
-                    if (resposta.linear === true){
-                        apagar.push(resposta.nome)
+                    if (resposta.linear === false){
+                        resposta.contador += 1
                     }
                 })
                 respostaDetalhes.linear = false
@@ -159,27 +228,123 @@ function proximaPergunta(){
     }
 
     if (perguntaAtual.tipo === 'homogenea'){
-        if (respostaAtual === 'A'){
-            respostas.forEach((resposta)=>{
-                if (resposta.homogenea === false){
-                    apagar.push(resposta.nome)
-                }
-            })
-            respostaDetalhes.homogenea = true
-        }
+        if (respostaDetalhes.linear === true){
+            if (respostaAtual === 'A'){
+                respostas.forEach((resposta)=>{
+                    if (resposta.homogenea === true){
+                        resposta.contador += 1
+                    }
+                })
+                respostaDetalhes.homogenea = true
+            }
 
-        if (respostaAtual === 'B'){
-            respostas.forEach((resposta)=>{
-                if (resposta.homogenea === true){
-                    apagar.push(resposta.nome)
-                }
-            })
-            respostaDetalhes.homogenea = false
+            if (respostaAtual === 'B'){
+                respostas.forEach((resposta)=>{
+                    if (resposta.homogenea === false){
+                        resposta.contador += 1
+                    }
+                })
+                respostaDetalhes.homogenea = false
+            }
         }
     }
-    apagar.forEach((nome)=>{
-        respostas = respostas.filter(resposta => resposta.nome !== nome)
-    })
+
+    if (perguntaAtual.tipo === 'coeficientes'){
+        if (respostaDetalhes.homogenea === false && respostaDetalhes.ordem === 1){
+            if (respostaAtual === 'A'){
+                respostas.forEach((resposta)=>{
+                    if (resposta.coeficientes === true){
+                        resposta.contador += 1
+                    }
+                })
+                respostaDetalhes.coeficientes = true
+            }
+
+            if (respostaAtual === 'B'){
+                respostas.forEach((resposta)=>{
+                    if (resposta.coeficientes === false){
+                        resposta.contador += 1
+                    }
+                })
+                respostaDetalhes.coeficientes = false
+            }
+        }
+    }
+
+    if (perguntaAtual.tipo === 'tipo'){
+        if (respostaDetalhes.linear === false && respostaDetalhes.ordem === 1){
+            if (respostaAtual === 'A'){
+                respostas.forEach((resposta)=>{
+                    if (resposta.ordem === 1 && resposta.linear === false){
+                        resposta.contador += 1
+                    }
+                })
+                respostaDetalhes.tipo = 'SEPARÁVEL'
+            }
+
+            if (respostaAtual === 'B'){
+                respostas.forEach((resposta)=>{
+                    if (resposta.ordem === 1 && resposta.linear === false){
+                        resposta.contador += 1
+                    }
+                })
+                respostaDetalhes.tipo = 'HOMOGÊNEA'
+            }
+
+            if (respostaAtual === 'C'){
+                respostas.forEach((resposta)=>{
+                    if (resposta.ordem === 1 && resposta.linear === false){
+                        resposta.contador += 1
+                    }
+                })
+                respostaDetalhes.tipo = 'EXATA'
+            }
+
+            if (respostaAtual === 'D'){
+                respostas.forEach((resposta)=>{
+                  if (resposta.ordem === 1 && resposta.linear === false){
+                        resposta.contador += 1
+                    }
+                })
+                respostaDetalhes.tipo = 'NÃO EXATA'
+            }
+        }
+    }
+
+    if (perguntaAtual.tipo === 'raizes'){
+        if (respostaDetalhes.linear === true && respostaDetalhes.homogenea === true && respostaDetalhes.ordem === 2){
+            if (respostaAtual === 'A'){
+                respostas.forEach((resposta)=>{
+                    if (resposta.ordem === 2 && resposta.linear === true && resposta.homogenea === true){
+                        resposta.contador += 1
+                    }
+                })
+                respostaDetalhes.raizes = 'DELTA > 0'
+            }
+
+            if (respostaAtual === 'B'){
+                respostas.forEach((resposta)=>{
+                    if (resposta.ordem === 2 && resposta.linear === true && resposta.homogenea === true){
+                        resposta.contador += 1
+                    }
+                })
+                respostaDetalhes.raizes = 'DELTA = 0'
+            }
+
+            if (respostaAtual === 'C'){
+                respostas.forEach((resposta)=>{
+                    if (resposta.ordem === 2 && resposta.linear === true && resposta.homogenea === true){
+                        resposta.contador += 1
+                    }
+                })
+                respostaDetalhes.raizes = 'DELTA < 0'
+            }
+
+        }
+    }
+
+    console.log(respostas)
+
     if (perguntaIndex === perguntas.length){
         mostrarResultado()
     } else {
